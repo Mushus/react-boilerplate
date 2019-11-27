6840c3c7d80779bbb45c2c0a00b20c09f0be3104
mushus <mushus.wynd@gmail.com>
2019-11-27 21:54:25 +0900

    add: webpack + typescript + eslint + prettier + react

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>hello wo rld!</h1>
+  </div>,
+  document.querySelector('#app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(jsx?|tsx?)/,
+        exclude: /(node_modules|dist)/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
